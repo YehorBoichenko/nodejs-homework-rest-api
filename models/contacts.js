68e4ea595aaadfd859587526d5d520a97d12afb3
Yehor Boichenko <boichenko.yehor@gmail.com>
2022-09-18 18:08:18 +0300

    hw2

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,60 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(contacts);
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const filterContact = contacts.find(({ id }) => id === contactId);
+  return filterContact || null;
+};
 
-const removeContact = async (contactId) => {}
+const updatedContacts = async (contacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const contactsIndex = contacts.findIndex(({ id }) => id === contactId);
 
-const updateContact = async (contactId, body) => {}
+  if (index === -1) {
+    return null;
+  }
+
+  const [deletedContact] = contacts.splice(contactsIndex, 1);
+
+  await updatedContacts(contacts);
+
+  return deletedContact;
+};
+
+const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const userId = contacts.map((item) => +item.id);
+  const addNewContact = contacts.push({
+    name,
+    email,
+    phone,
+    id: String(Math.max(...userId) + 1),
+  });
+  await updatedContacts(contacts);
+  return addNewContact;
+};
+
+const updateContact = async (contactId, { name, email, phone }) => {
+  const contacts = await listContacts();
+  const contactsIndex = contacts.findIndex(({ id }) => id === contactId);
+  if (contactsIndex === -1) {
+    return null;
+  }
+  contacts[contactsIndex] = { contactId, name, email, phone };
+  await updatedContacts(contacts);
+  return contacts[contactsIndex];
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +62,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
