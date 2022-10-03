7b1ea17338ee486e574f7e5c64bca3b663f197e2
Yehor Boichenko <boichenko.yehor@gmail.com>
2022-10-03 16:57:28 +0300

    add some extra notifications

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -18,6 +18,9 @@ const login = async (req, res) => {
     id: user._id,
   };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+  if (error) {
+    throw RequestError(400, "missing required fields");
+  }
   await User.findByIdAndUpdate(user._id, { token });
   res.json({
     token,
--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -8,6 +8,9 @@ const signup = async (req, res) => {
   if (user) {
     throw RequestError(409, "Email in use");
   }
+  if (user) {
+    throw RequestError(400, "missing required fields");
+  }
   const hashPassword = await bcrypt.hash(password, 10);
   const result = await User.create({ ...req.body, password: hashPassword });
   res.status(201).json({
